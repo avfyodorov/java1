7e170738d4fb51220bd318f45a2ff16db31daf5c
Александр <avfyodorov60@yandex.ru>
2020-02-03 15:47:03 +0300

    Test1 1

---
+++ src/ru/progwards/java1/lessons/test/Test1.java
@@ -0,0 +1,9 @@
+package ru.progwards.java1.lessons.test;
+
+public class Test1
+{
+  public static void main(String[] args)
+  {
+    System.out.print("Сделаю коммит, запушу в репо: робот, проверяй теперь всё это...");
+  }
+}
